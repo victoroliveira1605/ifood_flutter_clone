7cc39f65855d795c2708120619e38ec80dd03227
Victor Oliveira <victorhgo.rp@gmail.com>
2021-03-21 14:02:46 -0300

    tab bottom

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,5 +4,6 @@ class AppColors {
   static const Color primaryColor = Colors.red;
   static const Color titleColor = Color(0xFFB7B7B7);
   static const Color black = Colors.black;
+  static const Color white = Colors.white;
   static const Color black54 = Colors.black54;
 }
--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ifood_flutter_clone/core/theme/app_icons.dart';
+import 'components/bottom_navigator_component.dart';
 import 'components/tab_bar_component.dart';
 import 'components/header_local_component.dart';
 
@@ -10,28 +12,83 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   late TabController tabController;
+
   @override
   void initState() {
     tabController = TabController(length: 2, vsync: this);
     super.initState();
   }
 
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-          children: [
-            HeaderLocationComponent(
-              location: 'R. Daniel Ferranti, 455',
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                HeaderLocationComponent(
+                  location: 'Rua das Flores do Campo, 10',
+                ),
+                TabBarComponent(
+                  controller: tabController,
+                  onTap: (index) {},
+                ),
+              ],
             ),
-            TabBarComponent(
-              controller: tabController,
-              onTap: (index) {},
-            ),
-          ],
-        ),
+          ),
+          BottomNavigatorComponent(
+            onTap: (index) {
+              switch (index) {
+                case 0:
+                  print('Inicio');
+                  break;
+                case 1:
+                  print('Busca');
+                  break;
+                case 2:
+                  print('Pedidos');
+                  break;
+                case 3:
+                  print('Perfil');
+                  break;
+                default:
+              }
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            currentIndex: _currentIndex,
+            items: [
+              BottomNavigatorItemComponent(
+                label: 'Início',
+                activeIcon: AppIcons.homeActive,
+                icon: AppIcons.home,
+                onTap: () {},
+              ),
+              BottomNavigatorItemComponent(
+                label: 'Busca',
+                activeIcon: AppIcons.searchActive,
+                icon: AppIcons.search,
+                onTap: () {},
+              ),
+              BottomNavigatorItemComponent(
+                label: 'Pedidos',
+                activeIcon: AppIcons.ordersActive,
+                icon: AppIcons.orders,
+                onTap: () {},
+              ),
+              BottomNavigatorItemComponent(
+                label: 'Perfil',
+                activeIcon: AppIcons.profileActive,
+                icon: AppIcons.profile,
+                onTap: () {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
